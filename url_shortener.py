d8a239de23a828c260a00790f6b871255aab672e
ddhung <ddhung@ambition.vn>
2020-09-07 17:58:37 +0700

    [add] url_shorten function, sample urls

---
+++ url_shortener.py
@@ -0,0 +1,24 @@
+#Importing the required libraries
+from __future__ import with_statement
+import contextlib
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+import sys
+
+def make_shorten(url):
+    request_url = ('http://tinyurl.com/api-create.php?' + urlencode({'url':url}))
+    with contextlib.closing(urlopen(request_url)) as response:
+        return response.read().decode('utf-8')
+
+def main():
+    for shortyurl in map(make_shorten,sys.argv[1:]):
+        print(shortyurl)
+
+if __name__ == '__main__':
+    main()
